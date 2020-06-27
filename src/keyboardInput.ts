16cf36a0f1430235549001514d7061aa64b8ebdd
YAN Yuchen <cirahnitex@gmail.com>
2020-06-27 17:58:53 +0800

    added typestring

--- src/keyboardInput.ts
+++ src/keyboardInput.ts
@@ -11,6 +11,8 @@ import {
     mouseX1Down, mouseX1Up,
     mouseX2Down, mouseX2Up
 } from "./mouse";
+import * as K from "./keycodes"
+
 const arch = require("os").arch();
 
 const Input = StructType({
@@ -84,4 +86,109 @@ export function keyUp(keyCode: number) {
 export function keyPress(keyCode: number) {
     keyDown(keyCode);
     keyUp(keyCode);
+}
+
+const charToKeyCode:Record<string, ()=>void> = {
+    "0": ()=>keyPress(K.KEY_0),
+    "1": ()=>keyPress(K.KEY_1),
+    "2": ()=>keyPress(K.KEY_2),
+    "3": ()=>keyPress(K.KEY_3),
+    "4": ()=>keyPress(K.KEY_4),
+    "5": ()=>keyPress(K.KEY_5),
+    "6": ()=>keyPress(K.KEY_6),
+    "7": ()=>keyPress(K.KEY_7),
+    "8": ()=>keyPress(K.KEY_8),
+    "9": ()=>keyPress(K.KEY_9),
+    "!":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_1);keyUp(K.KEY_LSHIFT);},
+    "@":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_2);keyUp(K.KEY_LSHIFT);},
+    "#":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_3);keyUp(K.KEY_LSHIFT);},
+    "$":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_4);keyUp(K.KEY_LSHIFT);},
+    "%":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_5);keyUp(K.KEY_LSHIFT);},
+    "^":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_6);keyUp(K.KEY_LSHIFT);},
+    "&":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_7);keyUp(K.KEY_LSHIFT);},
+    "*":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_8);keyUp(K.KEY_LSHIFT);},
+    "(":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_9);keyUp(K.KEY_LSHIFT);},
+    ")":()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_0);keyUp(K.KEY_LSHIFT);},
+    "a": ()=>keyPress(K.KEY_A),
+    "b": ()=>keyPress(K.KEY_B),
+    "c": ()=>keyPress(K.KEY_C),
+    "d": ()=>keyPress(K.KEY_D),
+    "e": ()=>keyPress(K.KEY_E),
+    "f": ()=>keyPress(K.KEY_F),
+    "g": ()=>keyPress(K.KEY_G),
+    "h": ()=>keyPress(K.KEY_H),
+    "i": ()=>keyPress(K.KEY_I),
+    "j": ()=>keyPress(K.KEY_J),
+    "k": ()=>keyPress(K.KEY_K),
+    "l": ()=>keyPress(K.KEY_L),
+    "m": ()=>keyPress(K.KEY_M),
+    "n": ()=>keyPress(K.KEY_N),
+    "o": ()=>keyPress(K.KEY_O),
+    "p": ()=>keyPress(K.KEY_P),
+    "q": ()=>keyPress(K.KEY_Q),
+    "r": ()=>keyPress(K.KEY_R),
+    "s": ()=>keyPress(K.KEY_S),
+    "t": ()=>keyPress(K.KEY_T),
+    "u": ()=>keyPress(K.KEY_U),
+    "v": ()=>keyPress(K.KEY_V),
+    "w": ()=>keyPress(K.KEY_W),
+    "x": ()=>keyPress(K.KEY_X),
+    "y": ()=>keyPress(K.KEY_Y),
+    "z": ()=>keyPress(K.KEY_Z),
+    "A": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_A);keyUp(K.KEY_LSHIFT);},
+    "B": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_B);keyUp(K.KEY_LSHIFT);},
+    "C": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_C);keyUp(K.KEY_LSHIFT);},
+    "D": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_D);keyUp(K.KEY_LSHIFT);},
+    "E": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_E);keyUp(K.KEY_LSHIFT);},
+    "F": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_F);keyUp(K.KEY_LSHIFT);},
+    "G": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_G);keyUp(K.KEY_LSHIFT);},
+    "H": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_H);keyUp(K.KEY_LSHIFT);},
+    "I": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_I);keyUp(K.KEY_LSHIFT);},
+    "J": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_J);keyUp(K.KEY_LSHIFT);},
+    "K": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_K);keyUp(K.KEY_LSHIFT);},
+    "L": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_L);keyUp(K.KEY_LSHIFT);},
+    "M": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_M);keyUp(K.KEY_LSHIFT);},
+    "N": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_N);keyUp(K.KEY_LSHIFT);},
+    "O": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_O);keyUp(K.KEY_LSHIFT);},
+    "P": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_P);keyUp(K.KEY_LSHIFT);},
+    "Q": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_Q);keyUp(K.KEY_LSHIFT);},
+    "R": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_R);keyUp(K.KEY_LSHIFT);},
+    "S": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_S);keyUp(K.KEY_LSHIFT);},
+    "T": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_T);keyUp(K.KEY_LSHIFT);},
+    "U": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_U);keyUp(K.KEY_LSHIFT);},
+    "V": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_V);keyUp(K.KEY_LSHIFT);},
+    "W": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_W);keyUp(K.KEY_LSHIFT);},
+    "X": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_X);keyUp(K.KEY_LSHIFT);},
+    "Y": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_Y);keyUp(K.KEY_LSHIFT);},
+    "Z": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_Z);keyUp(K.KEY_LSHIFT);},
+    "`": ()=>keyPress(K.KEY_GRAVE),
+    "~": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_GRAVE);keyUp(K.KEY_LSHIFT);},
+    "-": ()=>keyPress(K.KEY_MINUS),
+    "_": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_MINUS);keyUp(K.KEY_LSHIFT);},
+    "=": ()=>keyPress(K.KEY_EQUAL),
+    "+": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_EQUAL);keyUp(K.KEY_LSHIFT);},
+    "[": ()=>keyPress(K.KEY_LBRACKET),
+    "{": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_LBRACKET);keyUp(K.KEY_LSHIFT);},
+    "]": ()=>keyPress(K.KEY_RBRACKET),
+    "}": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_RBRACKET);keyUp(K.KEY_LSHIFT);},
+    "\\": ()=>keyPress(K.KEY_BACKSLASH),
+    "|": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_BACKSLASH);keyUp(K.KEY_LSHIFT);},
+    ";": ()=>keyPress(K.KEY_SEMICOLON),
+    ":": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_SEMICOLON);keyUp(K.KEY_LSHIFT);},
+    "'": ()=>keyPress(K.KEY_QUOTE),
+    "\"": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_QUOTE);keyUp(K.KEY_LSHIFT);},
+    ",": ()=>keyPress(K.KEY_COMMA),
+    "<": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_COMMA);keyUp(K.KEY_LSHIFT);},
+    ".": ()=>keyPress(K.KEY_PERIOD),
+    ">": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_PERIOD);keyUp(K.KEY_LSHIFT);},
+    "/": ()=>keyPress(K.KEY_SLASH),
+    "?": ()=>{keyDown(K.KEY_LSHIFT);keyPress(K.KEY_SLASH);keyUp(K.KEY_LSHIFT);},
+
+};
+
+export function typeString(str: string) {
+    for(const char of str) {
+        const f = charToKeyCode[char];
+        if(f) f();
+    }
 }
--- src/scratch.ts
+++ src/scratch.ts
@@ -1,9 +1,9 @@
 import {captureScreen, getScreenSize} from "./screen";
 import {getMousePos} from "./mouse";
-import {keyPress} from "./keyboardInput";
+import {keyPress, typeString} from "./keyboardInput";
 import {KEY_A} from "./keycodes";
 import {addKeyDownListener} from "./keyboardEvent";
 
 addKeyDownListener(KEY_A, ()=>{
-    console.log('a pressed');
+    typeString("Hello, World!")
 });
